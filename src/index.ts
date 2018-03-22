c05ab51d6fc7c62c3d19ca12272127ff237e8030
Bo Peng <ben.bog@gmail.com>
2018-03-21 21:04:02 -0500

    Add a add language selector button, which does not work right

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,23 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {
+  IDisposable, DisposableDelegate
+} from '@phosphor/disposable';
+
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+
 import '../style/index.css';
 
 
@@ -21,10 +38,10 @@ function registerSoSFileType(app) {
 }
 
 function registerSoSCodeMirrorMode() {
-  /* We should use 
+  /* We should use
    *   import * as CodeMirror from 'codemirror' directly. However, the typefile
    *   from @types/codemirror https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/codemirror/index.d.ts
-   *   is different from the one used by Jupyter 
+   *   is different from the one used by Jupyter
    *   (https://github.com/jupyterlab/jupyterlab/blob/3b4c1a3df53b7446516a4cb1138cc57ae91a7b80/packages/codemirror/typings/codemirror/codemirror.d.ts)
    * so I would get errors such as property "defineMIME" does not exist etc.
    *
@@ -54,6 +71,46 @@ function registerSoSCodeMirrorMode() {
   });
 }
 
+function is_sos(panel: NotebookPanel) {
+    // when a new notebook is created with a sos kernel, session.kernelPreference would be sos
+    // or an existing notebook with sos kernel is opened, model.metadata.kernelspec would be sos
+    console.log(panel.context.session.kernelPreference.name);
+    console.log(panel.notebook.model.metadata.get('kernelspec'));
+    return panel.context.session.kernelPreference.name === 'sos' || panel.notebook.model.metadata.get('kernelspec')['name'] === 'sos';
+}
+
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.notebook, context.session);
+    };
+    if (!is_sos(panel))
+        return;
+
+    let button = new ToolbarButton({
+      className: 'myButton',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
+
+    let i = document.createElement('i');
+    i.classList.add('fa', 'fa-fast-forward');
+    button.node.appendChild(i);
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+function addLanguageSelector(app: JupyterLab) {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+}
 /**
  * Initialization data for the sos-extension extension.
  */
@@ -63,6 +120,7 @@ const extension: JupyterLabPlugin<void> = {
   activate: (app: JupyterLab) => {
     registerSoSCodeMirrorMode();
     registerSoSFileType(app);
+    addLanguageSelector(app);
     console.log('JupyterLab extension sos-extension is activated!');
   }
 };
