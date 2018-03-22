92b9800cad8fdc47e506f0af92e43600d89f0663
Bo Peng <ben.bog@gmail.com>
2018-03-21 23:41:44 -0500

    Add language dropdown button only for sos notebook

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {
-  JupyterLab, JupyterLabPlugin
+  JupyterLab,
+  JupyterLabPlugin
 } from '@jupyterlab/application';
 
 import {
-  IDisposable, DisposableDelegate
+  IDisposable,
+  DisposableDelegate
 } from '@phosphor/disposable';
 
 import {
@@ -15,13 +17,15 @@ import {
 } from '@jupyterlab/docregistry';
 
 import {
-  NotebookActions, NotebookPanel, INotebookModel
+  NotebookActions,
+  NotebookPanel,
+  INotebookModel
 } from '@jupyterlab/notebook';
 
 
 import '../style/index.css';
 
-
+import * as $ from "jquery";
 import * as codemirror_type from 'codemirror';
 import 'codemirror/mode/meta';
 import 'codemirror/mode/python/python';
@@ -47,9 +51,9 @@ function registerSoSCodeMirrorMode() {
    *
    * The temporary walkaround is to define CodeMirror as any type and stop tsc from generating such errors.
    **/
-  let CodeMirror : any = codemirror_type;
+  let CodeMirror: any = codemirror_type;
 
-  CodeMirror.defineMode('sos', (config: CodeMirror.EditorConfiguration, modeOptions?: any) => {
+  CodeMirror.defineMode('sos', (config: CodeMirror.EditorConfiguration, modeOptions ? : any) => {
     let pythonConf: any = {};
     for (let prop in modeOptions) {
       if (modeOptions.hasOwnProperty(prop)) {
@@ -71,37 +75,64 @@ function registerSoSCodeMirrorMode() {
   });
 }
 
-function is_sos(panel: NotebookPanel) {
+/*
+function is_sos(context: DocumentRegistry.IContext<INotebookModel>) {
     // when a new notebook is created with a sos kernel, session.kernelPreference would be sos
     // or an existing notebook with sos kernel is opened, model.metadata.kernelspec would be sos
     console.log(panel.context.session.kernelPreference.name);
     console.log(panel.notebook.model.metadata.get('kernelspec'));
-    return panel.context.session.kernelPreference.name === 'sos' || panel.notebook.model.metadata.get('kernelspec')['name'] === 'sos';
+    return  || panel.notebook.model.metadata.get('kernelspec')['name'] === 'sos';
+}
+*/
+
+function displayButtons(panel: NotebookPanel) {}
 }
 
 export
-class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+class ButtonExtension implements DocumentRegistry.IWidgetExtension < NotebookPanel, INotebookModel > {
   /**
    * Create a new extension object.
    */
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext < INotebookModel > ): IDisposable {
     let callback = () => {
       NotebookActions.runAll(panel.notebook, context.session);
     };
-    if (!is_sos(panel))
-        return;
+
+    // kernel might not be available at this time (e.g. when we open an existing notebook)
+    // in this case, a button will be added. However, the button will be hidden and only 
+    let nb_kernel = context.session.kernelPreference.name;
+    if (nb_kernel && nb_kernel !== 'sos') {
+      return;
+    }
 
     let button = new ToolbarButton({
-      className: 'myButton',
+      className: 'sos_button',
       onClick: callback,
       tooltip: 'Run All'
     });
 
     let i = document.createElement('i');
+
     i.classList.add('fa', 'fa-fast-forward');
     button.node.appendChild(i);
 
     panel.toolbar.insertItem(0, 'runAll', button);
+
+    if (!nb_kernel) {
+      // if kernel information is not available, the buttons will be removed
+      // when the kernel is ready. New buttons will not be created because by
+      // that time the kernel information is clear
+      context.session.ready.then(
+        () => {
+          displayButtons(panel);
+          let cur_kernel = panel.context.session.kernelPreference.name;
+          if (cur_kernel !== 'sos') {
+            // if this is not a sos kernel, remove all buttons
+            $('.sos_button', panel.node).remove();
+          }
+        }
+      );
+    }
     return new DisposableDelegate(() => {
       button.dispose();
     });
@@ -111,10 +142,12 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
 function addLanguageSelector(app: JupyterLab) {
   app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
 }
+
+
 /**
  * Initialization data for the sos-extension extension.
  */
-const extension: JupyterLabPlugin<void> = {
+const extension: JupyterLabPlugin < void > = {
   id: 'sos-extension',
   autoStart: true,
   activate: (app: JupyterLab) => {
