101794344a822002e0e82d4a9ef97b83c9a42b6e
Bo Peng <ben.bog@gmail.com>
2018-03-25 09:41:24 -0500

    Start with the JS version

---
+++ src/codemirror-sos.js
@@ -0,0 +1,49 @@
+"use strict";
+exports.__esModule = true;
+var CodeMirror = require("codemirror");
+require("codemirror/mode/meta");
+require("codemirror/mode/python/python");
+var SOS_MIME_TYPE = 'text/x-sos';
+/* This is temporarily copied from JupyterLab ipython mode. Will be redefined
+ * for SoS when the code works.
+ */
+CodeMirror.defineMode('sos', function (config, modeOptions) {
+    // languages should be {open, close, mode [, delimStyle] [, innerStyle]} objects
+    var python_mode = CodeMirror.getMode(config, 'python');
+    return {
+        startState: function () {
+            return {
+                sos: {
+                    state: null
+                },
+                innerActive: null,
+                inner: null
+            };
+        },
+        copyState: function (state) {
+            return {
+                sos: state.sos,
+                innerActive: state.innerActive,
+                inner: state.innerActive && CodeMirror.copyState(state.innerActive.mode, state.inner)
+            };
+        },
+        token: function (stream, state) {
+            // if in SoS mode
+            if (!state.innerActive) {
+                return null;
+            }
+            else {
+                return null;
+            }
+        }
+    };
+});
+CodeMirror.defineMIME(SOS_MIME_TYPE, 'sos');
+CodeMirror.modeInfo.push({
+    ext: ['sos'],
+    mime: SOS_MIME_TYPE,
+    mode: 'sos',
+    name: 'SoS'
+});
+// for testing, let us call runmode on SoS textarea
+console.log("SoS Code mirror mode registered.");
