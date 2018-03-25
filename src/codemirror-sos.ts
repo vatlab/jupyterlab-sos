7cce9b0af2aa011e00f82989dbcf998c256e379b
Bo Peng <ben.bog@gmail.com>
2018-03-25 11:50:48 -0500

    Basic SoS Mode based on python. Will be rewritten for SoS

--- src/codemirror-sos.ts
+++ src/codemirror-sos.ts
@@ -1,56 +1,14 @@
 import * as CodeMirror from 'codemirror';
 
 import 'codemirror/mode/meta';
-import 'codemirror/mode/python/python';
 
-const SOS_MIME_TYPE = 'text/x-sos';
+// this module defines codemirror mode of SoS. It is written in JS because it
+// will be used by jupyter sos-notebook as well.
+import 'sos-mode.js'
 
-/* This is temporarily copied from JupyterLab ipython mode. Will be redefined
- * for SoS when the code works.
- */
-CodeMirror.defineMode('sos', (config: CodeMirror.EditorConfiguration, modeOptions?: any) => {
-    // languages should be {open, close, mode [, delimStyle] [, innerStyle]} objects
-    var python_mode = CodeMirror.getMode(config, 'python');
-
-    return {
-        startState: function() {
-            return {
-                sos: {
-                    state: null,
-                },
-                innerActive: null,
-                inner: null
-            };
-        },
-
-        copyState: function(state) {
-            return {
-                sos: state.sos,
-                innerActive: state.innerActive,
-                inner: state.innerActive && CodeMirror.copyState(state.innerActive.mode, state.inner)
-            };
-        },
-
-        token: function(stream, state) {
-            // if in SoS mode
-            if (!state.innerActive) {
-                return null;
-            } else {
-                return null;
-            }
-        },
-    };
-});
-
-
-CodeMirror.defineMIME(SOS_MIME_TYPE, 'sos');
 CodeMirror.modeInfo.push({
     ext: ['sos'],
-    mime: SOS_MIME_TYPE,
+    mime: "text/x-sos",
     mode: 'sos',
     name: 'SoS'
 });
-
-// for testing, let us call runmode on SoS textarea
-
-console.log("SoS Code mirror mode registered.")
