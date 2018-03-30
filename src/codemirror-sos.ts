992a7f836a6dddb807362dc013960f17dd17798d
Bo Peng <ben.bog@gmail.com>
2018-03-30 14:46:29 -0500

    Re-work overlay magic to support multipline interpolation vatlab/sos-notebook#52

--- src/codemirror-sos.ts
+++ src/codemirror-sos.ts
@@ -76,12 +76,16 @@ function findMode(mode) {
     return null;
 }
 
-function markExpr(sigil, python_mode) {
+function markExpr(python_mode) {
     return {
         startState: function() {
             return {
                 in_python: false,
-                end_pos: null,
+                sigil: {
+                    left: '{',
+                    right: '}'
+                },
+                matched: true,
                 python_state: CodeMirror.startState(python_mode),
             };
         },
@@ -89,66 +93,48 @@ function markExpr(sigil, python_mode) {
         copyState: function(state) {
             return {
                 in_python: state.in_python,
-                end_pos: state.end_pos,
+                sigil: state.sigil,
+                matched: state.matched,
                 python_state: CodeMirror.copyState(python_mode, state.python_state)
             };
         },
 
         token: function(stream, state) {
             if (state.in_python) {
-                if (stream.pos == state.end_pos - sigil.right.length) {
+                if (stream.match(state.sigil.right)) {
                     state.in_python = false;
-                    stream.pos += sigil.right.length;
                     return "sos-sigil";
                 }
                 let it = null;
                 try {
-                    it = python_mode.token(stream, state);
+                    it = python_mode.token(stream, state.python_state);
                 } catch (error) {
-                    console.log(error);
-                    state.in_python = false;
-                    stream.pos = state.end_pos;
-                    return "sos-interpolated";
+                    return "sos-interpolated error" + (state.matched ? "" : " sos-unmatched");
                 }
-                if (stream.pos >= state.end_pos)
-                    state.in_python = false;
                 if (it == 'variable' || it == 'builtin') {
                     let ct = stream.current();
                     // warn users in the use of input and output in {}
                     if (ct === 'input' || ct === 'output')
                         it += ' error';
                 }
-                return it ? ("sos-interpolated " + it) : "sos-interpolated";
+                return (it ? ("sos-interpolated " + it) : "sos-interpolated") + (state.matched ? "" : " sos-unmatched");
             } else {
-                if (sigil.left === '{' && sigil.right === '}') {
-                    // remove the double brace case
-                    if (stream.match(/\{\{[^{}}]*\}\}/))
-                        return null;
-                    if (stream.match(/\{[^{}}]*\}/)) {
-                        state.in_python = true;
-                        state.end_pos = stream.pos;
-                        stream.backUp(stream.current().length - 1);
-                        return "sos-sigil";
+                // remove the double brace case, the syntax highlighter
+                // does not have to worry (highlight) }}, although it would
+                // probably mark an error for single }
+                if (state.sigil.left === '{' && stream.match(/\{\{/))
+                    return null;
+                if (stream.match(state.sigil.left)) {
+                    state.in_python = true;
+                    // let us see if there is any right sigil till the end of the editor.
+                    try {
+                        state.matched = stream.lookAhead(5).includes(state.sigil.right);
+                    } catch (error) {
+                        // only codemirror 5.27.0 supports this function
                     }
-                } else if (sigil.left === '${' && sigil.right === '}') {
-                    if (stream.match(/\$\{[^}]*\}/)) {
-                        state.in_python = true;
-                        state.end_pos = stream.pos;
-                        stream.backUp(stream.current().length - 2);
-                        return "sos-sigil";
-                    }
-                } else {
-                    // string search
-                    if (stream.match(sigil.left)) {
-                        stream.eatWhile(x => x !== sigil.right);
-                        stream.pos += sigil.right.length;
-                        state.end_pos = stream.pos;
-                        state.in_python = true;
-                        stream.backUp(stream.current().length - 2);
-                        return "sos-sigil";
-                    }
+                    return "sos-sigil" + (state.matched ? "" : " sos-unmatched");
                 }
-                while (stream.next() && !stream.match(sigil.left, false)) { }
+                while (stream.next() && !stream.match(state.sigil.left, false)) { }
                 return null;
             }
         }
@@ -177,18 +163,20 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
         }
     }
     // if there is a user specified base mode, this is the single cell mode
+
+    // if there is a user specified base mode, this is the single cell mode
     if (base_mode) {
         var python_mode = CodeMirror.getMode({}, {
             name: 'python',
             version: 3
         });
+        var overlay_mode = markExpr(python_mode);
         return {
             startState: function() {
                 return {
-                    sos_sigil: null,
                     sos_mode: true,
                     base_state: CodeMirror.startState(base_mode),
-                    overlay_state: CodeMirror.startState(base_mode),
+                    overlay_state: CodeMirror.startState(overlay_mode),
                     // for overlay
                     basePos: 0,
                     baseCur: null,
@@ -200,10 +188,9 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
 
             copyState: function(state) {
                 return {
-                    sos_sigil: state.sos_sigil,
                     sos_mode: state.sos_mode,
                     base_state: CodeMirror.copyState(base_mode, state.base_state),
-                    overlay_state: CodeMirror.copyState(base_mode, state.overlay_state),
+                    overlay_state: CodeMirror.copyState(overlay_mode, state.overlay_state),
                     // for overlay
                     basePos: state.basePos,
                     baseCur: null,
@@ -228,19 +215,19 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                                 if (sosMagics[i] === "%expand") {
                                     // if there is no :, the easy case
                                     if (stream.eol() || stream.match(/\s*$/, false)) {
-                                        state.sos_sigil = {
+                                        state.overlay_state.sigil = {
                                             'left': '{',
                                             'right': '}'
                                         }
                                     } else {
                                         let found = stream.match(/\s+(\S+)\s+(\S+)$/, false);
                                         if (found) {
-                                            state.sos_sigil = {
+                                            state.overlay_state.sigil = {
                                                 'left': found[1],
                                                 'right': found[2]
                                             }
                                         } else {
-                                            state.sos_sigil = null;
+                                            state.overlay_state.sigil = null;
                                         }
                                     }
                                 }
@@ -255,7 +242,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                     }
                 }
 
-                if (state.sos_sigil) {
+                if (state.overlay_state.sigil) {
                     if (stream != state.streamSeen ||
                         Math.min(state.basePos, state.overlayPos) < stream.start) {
                         state.streamSeen = stream;
@@ -268,7 +255,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                     }
                     if (stream.start == state.overlayPos) {
                         stream.pos = stream.start;
-                        state.overlayCur = markExpr(state.sos_sigil, python_mode).token(stream, state.overlay_state);
+                        state.overlayCur = overlay_mode.token(stream, state.overlay_state);
                         state.overlayPos = stream.pos;
                     }
                     stream.pos = Math.min(state.basePos, state.overlayPos);
@@ -306,13 +293,13 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
     } else {
         // this is SoS mode
         base_mode = CodeMirror.getMode(conf, sosPythonConf);
+        overlay_mode = markExpr(base_mode);
         return {
             startState: function() {
                 return {
                     sos_state: null,
-                    sos_sigil: null,
                     base_state: CodeMirror.startState(base_mode),
-                    overlay_state: CodeMirror.startState(base_mode),
+                    overlay_state: CodeMirror.startState(overlay_mode),
                     inner_mode: null,
                     inner_state: null,
                     // for overlay
@@ -327,9 +314,8 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
             copyState: function(state) {
                 return {
                     sos_state: state.sos_state,
-                    sos_sigil: state.sos_sigil,
                     base_state: CodeMirror.copyState(base_mode, state.base_state),
-                    overlay_state: CodeMirror.copyState(base_mode, state.overlay_state),
+                    overlay_state: CodeMirror.copyState(overlay_mode, state.overlay_state),
                     inner_mode: state.inner_mode,
                     inner_state: state.inner_mode && CodeMirror.copyState(state.inner_mode, state.inner_state),
                     // for overlay
@@ -369,7 +355,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                     for (var i = 0; i < sosDirectives.length; i++) {
                         if (stream.match(sosDirectives[i])) {
                             // the rest of the lines will be processed as Python code
-                            state.sos_state = 'directive_option';
+                            state.sos_state = 'directive_option'
                             return "keyword strong";
                         }
                     }
@@ -377,7 +363,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                         if (stream.match(sosActions[i])) {
                             // switch to submode?
                             state.sos_state = 'start ' + stream.current().slice(0, -1);
-                            state.sos_sigil = null;
+                            state.overlay_state.sigil = null;
                             return "builtin strong";
                         }
                     }
@@ -426,7 +412,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                     // try to understand option expand=
                     if (stream.match(/expand\s*=\s*True/, false)) {
                         // highlight {}
-                        state.sos_sigil = {
+                        state.overlay_state.sigil = {
                             'left': '{',
                             'right': '}'
                         }
@@ -435,7 +421,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                         if (!found)
                             found = stream.match(/expand\s*=\s*'(\S+) (\S+)'/, false);
                         if (found)
-                            state.sos_sigil = {
+                            state.overlay_state.sigil = {
                                 'left': found[1],
                                 'right': found[2]
                             }
@@ -452,15 +438,15 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                             state.sos_state = 'nomanland';
                         }
                     }
-                    return token + ' sos-option';;
+                    return token + ' sos-option';
                 }
                 // can be start of line but not special
                 if (state.sos_state == 'nomanland') {
                     stream.skipToEnd();
                     return null;
                 } else if (state.inner_mode) {
-                    let it = 'sos_script';
-                    if (!state.sos_sigil) {
+                    let it = 'sos_script ';
+                    if (!state.overlay_state.sigil) {
                         let st = state.inner_mode.token(stream, state.inner_state);
                         return st ? it + st : null;
                     } else {
@@ -477,13 +463,13 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
                         }
                         if (stream.start == state.overlayPos) {
                             stream.pos = stream.start;
-                            state.overlayCur = markExpr(state.sos_sigil, base_mode).token(stream, state.overlay_state);
+                            state.overlayCur = overlay_mode.token(stream, state.overlay_state);
                             state.overlayPos = stream.pos;
                         }
                         stream.pos = Math.min(state.basePos, state.overlayPos);
                         // state.overlay.combineTokens always takes precedence over combine,
                         // unless set to null
-                        return (state.overlayCur ? state.overlayCur : state.baseCur) + " sos_script";
+                        return (state.overlayCur ? state.overlayCur : state.baseCur) + " sos-script";
                     }
                 } else {
                     return base_mode.token(stream, state.base_state);
@@ -493,10 +479,7 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
             indent: function(state, textAfter) {
                 // inner indent
                 if (state.inner_mode) {
-                    var mode = state.inner_mode.mode;
-                    if (!mode.indent) return CodeMirror.Pass;
-                    // inner mode will autoamtically indent + 4
-                    return mode.indent(state.inner_mode ? state.inner : state.outer, textAfter) + 4;
+                    return state.inner_mode.indent(state.inner_mode, textAfter) + 4;
                 } else {
                     return 0;
                 }
