6a987fc8f49500ac712099de6eca0840953c5bc7
Bo Peng <ben.bog@gmail.com>
2018-12-29 09:59:19 -0600

    Fix matching of section header

--- src/codemirror-sos.ts
+++ src/codemirror-sos.ts
@@ -343,10 +343,10 @@ CodeMirror.defineMode("sos", function(conf: CodeMirror.EditorConfiguration, pars
             // header, move to the end
             if (stream.match(/^\[.*\]$/, false)) {
               // if there is :
-              if (stream.match(/^\[[\s\w,]*:/)) {
+              if (stream.match(/^\[[\s\w_,-]+:/)) {
                 state.sos_state = 'header_option';
                 return "header line-section-header";
-              } else if (stream.match(/^\[\s\w,\]$/)) {
+              } else if (stream.match(/^\[[\s\w,-]+\]$/)) {
                 // reset state
                 state.sos_state = null;
                 state.inner_mode = null;
